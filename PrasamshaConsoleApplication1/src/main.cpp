4f6641647b7c28dc35bea706ea5c11c0574e29fa
PrasamshaAdhikari <167926352+PrasamshaAdhikari@users.noreply.github.com>
2024-08-27 10:55:20 +0545

    Update main.cpp

--- PrasamshaConsoleApplication1/src/main.cpp
+++ PrasamshaConsoleApplication1/src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
@@ -9,91 +10,117 @@
 #include "GameOver.h"
 
 using namespace sf;
-int W = 1280, H = 720;
+
+class Game {
+private:
+    RenderWindow window;
+    Paddle paddles[2];
+    Ball ball;
+    SoundBuffer buf_paddle, buf_wall, buf_score;
+    Sound paddleSound, wallSound, scoreSound;
+    Texture scText;
+    Sprite scoreSprite;
+    int W, H;
+
+public:
+    Game(int width, int height)
+        : window(VideoMode(width, height), "Pong!"), W(width), H(height) {
+        window.setKeyRepeatEnabled(true);
+        window.setVerticalSyncEnabled(false);
+
+        // Load Sounds
+        buf_paddle.loadFromFile("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\paddle.wav");
+        buf_wall.loadFromFile("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\wall.wav");
+        buf_score.loadFromFile("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\score.wav");
+        paddleSound.setBuffer(buf_paddle);
+        wallSound.setBuffer(buf_wall);
+        scoreSound.setBuffer(buf_score);
+
+        // Initialize Paddles
+        paddles[0].pos = Vector2f(20, 305);
+        paddles[1].pos = Vector2f(W - 40, 305);
+        paddles[0].initPaddles("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\stick_1.png");
+        paddles[1].initPaddles("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\stick_2.png");
+        paddles[0].speed = 1.f;
+        paddles[1].speed = 1.f;
+        paddles[0].dir = 2;
+        paddles[1].dir = 2;
+        paddles[0].score = 0;
+        paddles[1].score = 0;
+
+        // Initialize Ball
+        ball.pos = Vector2f(626, 346);
+        ball.initBall("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\ball.png");
+        ball.speed = Vector2f(0, 0);
+
+        // Initialize Score Text
+        scText.loadFromFile("C:\\Users\\MÃ¼cahit\\Desktop\\pong\\score.png");
+        scoreSprite.setTexture(scText);
+        scoreSprite.setPosition(546, 35);
+        createNumberSprites(window);
+    }
+
+    void run() {
+        while (window.isOpen()) {
+            processEvents();
+            update();
+            render();
+        }
+    }
+
+private:
+    void processEvents() {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
+                window.close();
+        }
+    }
+
+    void update() {
+        // Game Input Logic
+        handleInput();
+
+        // Start The Game By Pressing "SPACE"
+        if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            whoStartsTheGame(paddles[0], paddles[1], ball);
+        }
+
+        // Check Collision
+        Collision(ball, paddles[1], paddles[0], paddleSound, scoreSound, paddles[0].score, paddles[1].score);
+
+        paddles[0].movePaddles(ball);
+        paddles[1].movePaddles(ball);
+        ball.moveBall();
+        gameOver(ball, paddles[0], paddles[1]);
+        updateScore(paddles[0].score, paddles[1].score, window);
+    }
+
+    void handleInput() {
+        if (Keyboard::isKeyPressed(Keyboard::W)) paddles[0].dir = 0;
+        else if (Keyboard::isKeyPressed(Keyboard::S)) paddles[0].dir = 1;
+        else paddles[0].dir = 2;
+
+        if (Keyboard::isKeyPressed(Keyboard::Up)) paddles[1].dir = 0;
+        else if (Keyboard::isKeyPressed(Keyboard::Down)) paddles[1].dir = 1;
+        else paddles[1].dir = 2;
+    }
+
+    void render() {
+        window.clear(Color::Black);
+
+        // Draw Elements
+        paddles[0].drawPaddles(window);
+        paddles[1].drawPaddles(window);
+        ball.drawBall(window);
+        window.draw(scoreSprite);
+
+        window.display();
+    }
+};
 
 int main() {
-	// Sound Effects
-	SoundBuffer buf_0, buf_1, buf_2;
-	buf_0.loadFromFile("C:\\Users\\Mücahit\\Desktop\\pong\\paddle.wav");
-	buf_1.loadFromFile("C:\\Users\\Mücahit\\Desktop\\pong\\wall.wav");
-	buf_2.loadFromFile("C:\\Users\\Mücahit\\Desktop\\pong\\score.wav");
-	Sound paddle, wall, score;
-	paddle.setBuffer(buf_0);
-	wall.setBuffer(buf_1);
-	score.setBuffer(buf_2);
-
-	// Window
-	RenderWindow window(VideoMode(W, H), "Pong!");
-	window.setKeyRepeatEnabled(true);
-	window.setVerticalSyncEnabled(false);
-
-	// Initilaizing Paddles
-	Paddle paddles[2];
-	paddles[0].pos = Vector2f((20), (305));
-	paddles[1].pos = Vector2f((W-20-20), (305));
-	paddles[0].initPaddles("C:\\Users\\Mücahit\\Desktop\\pong\\stick_1.png");
-	paddles[1].initPaddles("C:\\Users\\Mücahit\\Desktop\\pong\\stick_2.png");
-	paddles[0].speed = 1.f;
-	paddles[1].speed = 1.f;
-	paddles[0].dir = 2;
-	paddles[1].dir = 2;
-	paddles[0].score = 0;
-	paddles[1].score = 0;
-
-	// Initializing Ball
-	Ball ball;
-	ball.pos = Vector2f((626), (346));
-	ball.initBall("C:\\Users\\Mücahit\\Desktop\\pong\\ball.png");
-	ball.speed = Vector2f(0,0);
-
-	// Score Text
-	Texture scText;
-	Sprite scoreSprite;
-	scText.loadFromFile("C:\\Users\\Mücahit\\Desktop\\pong\\score.png");
-	scoreSprite.setTexture(scText);
-	scoreSprite.setPosition((546), (35));
-	createNumberSprites(window);
-
-	while (window.isOpen())
-	{
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape)) window.close();
-		}
-
-		window.clear(Color::Black);
-
-		// Game Input Logic
-		if (Keyboard::isKeyPressed(Keyboard::W)) paddles[0].dir = 0;
-		else if (Keyboard::isKeyPressed(Keyboard::S)) paddles[0].dir = 1;
-		else paddles[0].dir = 2;
-
-		if (Keyboard::isKeyPressed(Keyboard::Up)) paddles[1].dir = 0;
-		else if (Keyboard::isKeyPressed(Keyboard::Down)) paddles[1].dir = 1;
-		else paddles[1].dir = 2;
-
-			// Start The Game By Pressing "SPACE"
-		if (Keyboard::isKeyPressed(Keyboard::Space))
-		{
-			whoStartsTheGame(paddles[0], paddles[1], ball);
-		}
-
-			// Check Collision
-		Collision(ball, paddles[1],paddles[0],paddle,score, paddles[0].score, paddles[1].score);
-
-		paddles[0].movePaddles(ball); 
-		paddles[1].movePaddles(ball);
-		ball.moveBall();
-		gameOver(ball, paddles[0], paddles[1]);
-		updateScore(paddles[0].score, paddles[1].score,window);
-		// Draw
-		paddles[0].drawPaddles(window);
-		paddles[1].drawPaddles(window);
-		ball.drawBall(window);
-		window.draw(scoreSprite);
-		
-		// Display
-		window.display();
-	}
+    Game pong(1280, 720);
+    pong.run();
+    return 0;
 }
